d5515c8ff254ecf6e69d0a1e9e38b4bfd808a8b4
sykes <sykesl@qq.com>
2023-10-24 16:51:39 +0800

    删了没必要的pinia state

--- docs/.vitepress/theme/store/ProductStore.js
+++ docs/.vitepress/theme/store/ProductStore.js
@@ -3,14 +3,8 @@ import { ref } from 'vue'
 
 export const useThemeStore = defineStore('theme', () => {
     const mode = ref(false)
-    const scrollTop = ref(0)
-    const scrollHeight = ref(0)
-    const clientHeight = ref(0)
 
     return {
-        mode,
-        scrollTop,
-        scrollHeight,
-        clientHeight
+        mode
     }
 })
